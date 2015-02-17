5e597c19c7b588d8a2f72d9e8290ea2b642a2c14
tuber <btu@thoughtworks.com>
2015-02-17 21:07:19 +0800

    [tuber]create Spring Frameworks.

---
+++ src/main/java/MessagePrinter.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2015/2/17.
+ */
+public class MessagePrinter {
+
+    public boolean sayHello() {
+        System.out.println("Hello World!");
+        return false;
+    }
+}
